e533b9c36b2baf8bc5ac946a6ed9c84a49894a5a
Raja-Mano <52944727+rajamanohar931@users.noreply.github.com>
2025-02-17 21:30:53 +0530

    stage url change

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ import { WebView } from 'react-native-webview';
 import * as ScreenOrientation from 'expo-screen-orientation';
 import * as NavigationBar from 'expo-navigation-bar';
 
-const WEB_URL = "https://stage.bougeegames.com";
+const WEB_URL = "https://stage.bougeegames.com/login";
 
 const App = () => {
   const [isFullScreen, setIsFullScreen] = useState(false);
